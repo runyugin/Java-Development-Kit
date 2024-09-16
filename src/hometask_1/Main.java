91508ece71660b87c40d53c3b393c57a45a22a3d
evgenijkrasnoperov <runyugin@mail.ru>
2024-09-16 15:46:29 +0300

    task_01

---
+++ src/hometask_1/Main.java
@@ -0,0 +1,10 @@
+package hometask_1;
+
+import java.io.IOException;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        new ConnectWindow(new LogHandler("./Hometask_1_Log"));
+    }
+}
